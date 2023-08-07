583fbad6982050d866af94a1e13812a0f5dd525d
Jinius36 <jinius0306@gmail.com>
2023-08-08 08:39:10 +0900

    feat : 스테이지 구현

--- Assets/Scripts/Scenes/GameScene1.cs
+++ Assets/Scripts/Scenes/GameScene1.cs
@@ -5,22 +5,25 @@ using UnityEngine.UI;
 
 public class GameScene1 : BaseScene
 {
-    [SerializeField] Vector3 characterSpawn = Vector3.zero;
-    [SerializeField] List<Vector3> EnemySpawnList;
+    [SerializeField] Vector3 characterSpawn;
+    //[SerializeField] List<Vector3> EnemySpawnList;
+    [SerializeField] GameObject Player;
     public override void Clear() { }
     protected override void Init()
     {
         base.Init();
+        characterSpawn = new Vector3(52.0f, 2.0f, 0.0f);
         SceneType = Define.Scene.GameScene1;
         Managers.UI.ShowSceneUI<UI_GameScene>();
-        Managers.Resource.Instantiate("Character").transform.position = characterSpawn;
         Managers.Resource.Instantiate("Maps/stage1");
+        Player = Managers.Resource.Instantiate("Character");
+        Player.transform.position = characterSpawn;
         Managers.Resource.Instantiate("Enemy");
     }
     public void ReStart()
     {
         Managers.UI.ShowSceneUI<UI_GameScene>();
-        Managers.Resource.Instantiate("Character").transform.position = characterSpawn;
+        Managers.Resource.Instantiate("Character", characterSpawn);
         Managers.Resource.Instantiate("Maps/stage1");
         Managers.Resource.Instantiate("Enemy");
     }
