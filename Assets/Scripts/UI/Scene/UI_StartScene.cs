089153cdfa5bcc4dc6ac73ca4c22340b24999275
Jinius36 <jinius0306@gmail.com>
2023-08-07 18:16:29 +0900

    Scene 구현 시작

---
+++ Assets/Scripts/UI/Scene/UI_StartScene.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_StartScene : UI_Scene
+{
+    public enum Buttons
+    {
+        GameStart,
+        Setting
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+    public override void Init()
+    {
+        base.Init();
+        Bind<Button>(typeof(Buttons));
+        GetButton((int)Buttons.GameStart).gameObject.AddUIEvent(StartClick);
+        GetButton((int)Buttons.Setting).gameObject.AddUIEvent(SettingClick);
+    }
+    // Update is called once per frame
+    public void StartClick(PointerEventData data)
+    {
+        Managers.Scene.LoadScene(Define.Scene.GameScene);
+    }
+    public void SettingClick(PointerEventData data)
+    {
+        //Managers.UI.ShowPopUpUI<UI_Setting>();
+    }
+    void Update()
+    {
+        
+    }
+}
