ec903fc9270cc63cf958b7c0dd74bbccbecbdfcd
hans4809 <58892107+hans4809@users.noreply.github.com>
2023-09-25 12:44:37 +0900

    feat : 에디터리스트 추가해봣는데 버그가 남

---
+++ Assets/Scripts/ListTester.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListTester : MonoBehaviour
+{
+    public int[] integers;
+
+    public Vector3[] vectors;
+
+    public ColorPoint[] colorPoints;
+
+    public Transform[] objects;
+}
